274163097332e09c4b33ee0b3184b136bd3f9b2f
tanapi-e <ryota.wavershrinp@icloud.com>
2020-10-22 00:08:39 +0900

    practice

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println()
+	//fmt.Println()
+	calculation()
+}
+
+func calculation() {
+	var (
+		abcd = 23
+		efgh = 21
+		aiu  int
+	)
+
+	if abcd > 20 {
+		aiu = abcd + efgh
+	}
+
+	fmt.Println(aiu)
 }
